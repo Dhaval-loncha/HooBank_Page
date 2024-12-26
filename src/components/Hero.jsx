f02451bb9a59f00ae5ebf4737cbca8a9add13c66
Loncha Dhaval <163704413+Dhaval-loncha@users.noreply.github.com>
2024-12-26 17:57:37 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
 			</div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="robot" className="w-[100%] h-[100%] relative z-[5]" loading="lazy"/>
+        <img src={robot} alt="robot" className="w-[100%] h-[100%] relative z-[5]"/>
 
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
         <div className="absolute z-[1] w-[80%] h-[85%] rounded-full bottom-40 white__gradient"></div>
